dbf96c02a01cee1d0beb0f3edc96e61ad196b0d5
JungHoon <junghoon.sin@nts-corp.com>
2021-10-04 12:24:59 +0900

    <feat> : 기본 흐름 & 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import nextstep.utils.Console;
+
 public class Application {
+    private static String answer = "111";
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        boolean isIng = true;
+
+        while (isIng) {
+            System.out.print("숫자를 입력해주세요 :");
+            String input = Console.readLine();
+
+            String result = getResult(input);
+            if (result.equals("3스트라이크")) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+                isIng = false;
+            }
+        }
+    }
+
+    private static String getResult(String input) {
+        if (input.equals(answer)) {
+            return "3스트라이크";
+        }
+
+        return "3볼";
     }
 }
