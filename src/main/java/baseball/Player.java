2e69b486c90ab98cf0335130f564434d4cdc5e0d
JungHoon <junghoon.sin@nts-corp.com>
2021-10-04 13:50:51 +0900

    <feat> 입력받기 - 밸리데이션 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -2,16 +2,35 @@ package baseball;
 
 import nextstep.utils.Console;
 
+import java.util.*;
+import java.util.regex.Pattern;
+
 public class Player {
+    private final String threeDigitExp = "^[0-9]{3}";
 
     public String play() {
         String input = Console.readLine();
         validate(input);
-
         return input;
     }
 
     private void validate(String input) {
-        
+        if (!Pattern.matches(threeDigitExp, input)) {
+            throw new IllegalArgumentException("[ERROR] 0~9 3자리 숫자만 입력 가능합니다.");
+        }
+        checkDuplicateInput(input);
     }
+
+    private void checkDuplicateInput(String input) {
+        List<String> list = Arrays.asList(input.split(""));
+        Set<String> set = new HashSet<>(list);
+
+        for (String str : set) {
+            if (Collections.frequency(list, str) > 1) {
+                throw new IllegalArgumentException("[ERROR] 서로 다른 수로 이루어진 3자리의 수로 입력하세요.");
+            }
+        }
+    }
+
+
 }
